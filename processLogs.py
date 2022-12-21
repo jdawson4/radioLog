47061ef55e5055fa2e6d221b8fd1888e85148487
Jacob Dawson <jacob.dawson@me.com>
2022-12-20 19:04:25 -0500

    initial commit

---
+++ processLogs.py
@@ -0,0 +1,5 @@
+# Author: Jacob Dawson
+#
+# All that we're doing in this file is taking the logs in toBeProcessed and
+# adding any unique stations (who have called CQ--that's important!) to the
+# big file named fullLog.txt
